0d1bd0a72b265d08ffd65d43b656498ae3329028
Erick Souza <esmerick@gmail.com>
2020-07-15 01:58:59 -0300

    Add page test

--- src/app/page-test/page-test-routing.module.ts
+++ src/app/page-test/page-test-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { FolderPage } from './folder.page';
+import { PageTestPage } from './page-test.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: FolderPage
-  },
-  {
-    path: 'Inbox',
-    component: FolderPage
+    component: PageTestPage
   }
 ];
 
@@ -18,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class FolderPageRoutingModule {}
+export class PageTestPageRoutingModule {}
